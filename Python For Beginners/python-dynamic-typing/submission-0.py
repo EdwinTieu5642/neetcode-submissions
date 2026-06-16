8ed721908f4690c8f63d049e7f08e21d3242b875
Edwin Tieu <edwintie@usc.edu>
2026-06-15 21:01:55 -0700

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,14 @@
+variable = 0
+print(type(variable))
+
+variable = 0.0
+print(type(variable))
+
+variable = True
+print(type(variable))
+
+variable = "hello"
+print(type(variable))
+
+variable = [1, 2, 3]
+print(type(variable))

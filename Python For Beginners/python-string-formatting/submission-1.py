669cd574138c00f13ff9ecfd43e17f89ce835869
Edwin Tieu <edwintie@usc.edu>
2026-06-21 19:38:06 -0700

    Add: python-string-formatting - submission-1

---
+++ Python For Beginners/python-string-formatting/submission-1.py
@@ -0,0 +1,9 @@
+def say_goodbye(name: str, hour: int) -> str:
+    new_str = f"Goodbye, {name}. See you again at {hour} o'clock."
+    return new_str    
+
+
+# do not modify below this line
+print(say_goodbye("Bob", 12))
+print(say_goodbye("Jane", 4))
+print(say_goodbye("NeetCode", 9))
